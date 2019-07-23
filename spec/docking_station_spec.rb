893738f3e91a966d1837b1bb308ac41f4ad63b87
Jamal Springer <jamal.springer@thomsonreuters.com>
2019-07-23 16:12:16 +0100

    updated spec file to only check if docking station exists,opposed to be able to exectue the release-bike method on it

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,6 +3,6 @@ require_relative '../lib/borris-bike.rb'
 describe 'DockingStation' do
 it 'Instantiates a new Docking station using the DockingStation method' do
   docking_station_1 = DockingStation.new
-  expect (docking_station_1).release_bike
+  expect (docking_station_1)
 end
 end
