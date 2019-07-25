55a5c23b535c24a9b53bfadc35e4946f849220ea
Jamal Springer <jamal.springer@thomsonreuters.com>
2019-07-25 14:00:48 +0100

    added test to check that bike released by release_bike method is an instance of Bike class

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,19 +1,25 @@
 require_relative '../lib/docking_station.rb'
+require_relative '../lib/bike.rb'
 
-  describe DockingStation do
+describe DockingStation do
   it 'Instantiates a new Docking station using the DockingStation method' do
     docking_station = DockingStation.new
     expect( docking_station).to be_kind_of(DockingStation)
   end
 
-    it 'Attempts to execute the release bike method on the said Docking station' do
-      docking_station = DockingStation.new
-      expect( docking_station).to respond_to(:release_bike)
-    end
-
-    it 'Test that the DockingStation.release_bike method creates an instance of Bike which can use the working? method' do
-      bike = DockingStation.new.release_bike
-      expect( bike).to be_working
-    end
-
+  it 'Attempts to execute the release bike method on the said Docking station' do
+    docking_station = DockingStation.new
+    expect( docking_station).to respond_to(:release_bike)
   end
+
+  it 'Test that the DockingStation.release_bike method creates an instance of Bike which can use the working? method' do
+    bike = DockingStation.new.release_bike
+    expect( bike).to be_working
+  end
+
+  it 'Tests that the release bike method returns an instance of bike class' do
+    bike = DockingStation.new.release_bike
+    expect( bike).to be_kind_of(Bike)
+  end
+
+end
