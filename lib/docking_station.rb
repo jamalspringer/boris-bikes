b2ef9fd89ad497bc1bdc960b8f1c16281e075943
Jamal Springer <jamal.springer@thomsonreuters.com>
2019-07-25 15:59:34 +0100

    Step15 - Added full? & empty? private methods

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,19 +1,38 @@
 require_relative '../lib/bike'
 
 class DockingStation
+
   attr_reader :bikes
   def initialize
   @bikes = []
   end
 
   def release_bike
-    raise 'Docking station is empty' if @bikes.size == 0
+    raise 'Docking station is empty' if empty?
     @bikes.shift
   end
 
   def dock(bike)
-    raise 'Docking station is full' if @bikes.size == 20
+    raise 'Docking station is full' if full?
     @bikes << bike
+  end
+
+  private
+
+  def full?
+    if @bikes.size >= 20
+      return true
+    else
+      return false
+    end
+  end
+
+  def empty?
+    if @bikes.size == 0
+      return true
+    else
+      return false
+    end
   end
 end
 
---
+++ ruby_tester.rb
@@ -0,0 +1,4 @@
+require './lib/docking_station.rb'
+
+docking_station = DockingStation.new
+docking_station.release_bike
